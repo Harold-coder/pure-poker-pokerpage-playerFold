93aa2d80af0d3aa00c5c11aa31f2b6c308b5185a
cheatingthemichal <52798998+cheatingthemichal@users.noreply.github.com>
2024-02-28 23:15:42 -0500

    removed all redundant helper functions

--- index.js
+++ index.js
@@ -1,254 +1,12 @@
 //index.js
 const AWS = require('aws-sdk');
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
-const Deck = require('Deck');
-const { evaluateBestHand, compareHands } = require('HandEvaluator');
+const Helper = require('UpdateGame');
 const gameTableName = process.env.GAME_TABLE;
 const connectionsTableName = process.env.CONNECTIONS_TABLE;
 const apiGatewayManagementApi = new AWS.ApiGatewayManagementApi({
     endpoint: process.env.WEBSOCKET_ENDPOINT
 });
-
-const GAME_STAGES = {
-    PRE_DEALING: 'preDealing',
-    PRE_FLOP: 'preFlop',
-    FLOP: 'flop',
-    TURN: 'turn',
-    RIVER: 'river',
-    GAME_OVER: 'gameOver'
-};
-
-function determineWinner(game) {
-    let remainingPot = game.pot;
-
-    // Create a temporary copy of pot contributions for calculations
-    let potContributionsCopy = game.players.map(player => ({
-        id: player.id,
-        potContribution: player.potContribution
-    }));
-
-    // Evaluate hands for all players and update their objects
-    game.players.forEach(player => {
-        const handResult = evaluateBestHand([...player.hand, ...game.communityCards]);
-        player.handDescription = handResult.description;
-        player.bestHand = handResult.bestHand;
-    });
-
-    let sortedPlayers = game.players.filter(p => p.inHand)
-        .sort((a, b) => potContributionsCopy.find(pc => pc.id === a.id).potContribution - potContributionsCopy.find(pc => pc.id === b.id).potContribution);
-
-    while (sortedPlayers.length > 0 && remainingPot > 0) {
-        const minContribution = potContributionsCopy.filter(pc => sortedPlayers.some(sp => sp.id === pc.id))[0].potContribution;
-        let potToDistribute = minContribution * sortedPlayers.length;
-        remainingPot -= potToDistribute;
-
-        const handEvaluations = sortedPlayers.map(player => ({
-            playerId: player.id,
-            bestHand: player.bestHand
-        }));
-
-        handEvaluations.sort((a, b) => compareHands(a.bestHand, b.bestHand));
-        const winners = handEvaluations.filter(e => compareHands(e.bestHand, handEvaluations[0].bestHand) === 0);
-
-        const share = potToDistribute / winners.length;
-        winners.forEach(winner => {
-            const player = game.players.find(p => p.id === winner.playerId);
-            player.chips += share;
-            player.amountWon = (player.amountWon || 0) + share;
-
-            if ((player.amountWon - player.potContribution) > 0 && !game.netWinners.includes(winner.playerId)) {
-                game.netWinners.push(winner.playerId);
-            }
-        });
-
-        // Deduct the distributed pot from the temporary pot contributions
-        potContributionsCopy.forEach(pc => {
-            if (sortedPlayers.some(sp => sp.id === pc.id)) {
-                pc.potContribution -= minContribution;
-            }
-        });
-
-        // Filter for the next iteration
-        sortedPlayers = sortedPlayers.filter(sp => potContributionsCopy.find(pc => pc.id === sp.id).potContribution > 0);
-    }
-
-    if (remainingPot > 0 && game.netWinners.length > 0) {
-        const remainingShare = remainingPot / game.netWinners.length;
-        game.netWinners.forEach(winnerId => {
-            const player = game.players.find(p => p.id === winnerId);
-            player.chips += remainingShare;
-            player.amountWon += remainingShare;
-        });
-    }
-
-    game.pot = 0;
-    game.gameStage = GAME_STAGES.GAME_OVER;
-    game.gameInProgress = false;
-    game.gameOverTimeStamp = new Date().toISOString();
-}
-
-function distributePotsForFolding(game) {
-    let updatedPlayers = [...game.players];
-    let totalPot = game.pot;
-
-    const remainingPlayerIndex = updatedPlayers.findIndex(player => player.inHand);
-
-    if (remainingPlayerIndex !== -1) {
-        updatedPlayers[remainingPlayerIndex].chips += totalPot;
-        updatedPlayers[remainingPlayerIndex].amountWon = (updatedPlayers[remainingPlayerIndex].amountWon || 0) + totalPot;
-    }
-
-    updatedPlayers.forEach(player => {
-        player.bet = 0;  // Reset bet for all players
-    });
-
-    // Update the game state
-    game.players = updatedPlayers;
-    game.pot = 0;
-    game.gameStage = GAME_STAGES.GAME_OVER;
-    game.netWinners.push(updatedPlayers[remainingPlayerIndex].id);  // Ensure to use 'id' not 'playerId'
-    game.gameInProgress = false;
-    game.gameOverTimeStamp = new Date().toISOString();
-}
-
-function checkAllInCondition(game) {
-    const activePlayers = game.players.filter(p => p.inHand);
-    const allInPlayers = activePlayers.filter(p => p.isAllIn);
-
-    if (allInPlayers.length === activePlayers.length) {
-        revealAllCommunityCards(game);
-        determineWinner(game);
-        return true; // Condition met, all players are all-in
-    } else if (allInPlayers.length === activePlayers.length - 1) {
-        const lastPlayer = activePlayers.find(p => !p.isAllIn);
-
-        // Check if the last player has matched the highest bet, is all-in, or has folded
-        if (lastPlayer.bet === game.highestBet) {
-            revealAllCommunityCards(game);
-            determineWinner(game);
-            return true; // Condition met, last player has responded appropriately
-        }
-    }
-
-    return false; // Condition not met, game should not advance yet
-}
-
-function revealAllCommunityCards(game) {
-    const cardsDealt = game.communityCards.length;
-    if (cardsDealt < 3) {
-        dealCommunityCards(game, 3 - cardsDealt);
-    }
-    if (cardsDealt < 4) {
-        dealCommunityCards(game, 4 - game.communityCards.length);
-    }
-    if (cardsDealt < 5) {
-        dealCommunityCards(game, 5 - game.communityCards.length);
-    }
-}
-
-function dealCommunityCards(game, count) {
-    // Check if game.deck is an instance of Deck, if not, reinstantiate it
-    if (!(game.deck instanceof Deck)) {
-        const deck = new Deck();
-        deck.cards = game.deck.cards; // Assuming game.deck.cards holds the array of cards
-        game.deck = deck;
-    }
-
-    const cards = game.deck.deal(count);
-    game.communityCards.push(...cards);
-}
-
-function advanceTurn(game) {
-    let nextTurn = (game.currentTurn + 1) % game.players.length;
-    while (!game.players[nextTurn].inHand || game.players[nextTurn].chips === 0) {
-        nextTurn = (nextTurn + 1) % game.players.length;
-    }
-
-    // Update the game state with the new current turn
-    game.currentTurn = nextTurn;
-}
-
-function findFirstActivePlayer(game) {
-    let startIndex = game.smallBlindIndex
-    for (let i = 0; i < game.playerCount; i++) {
-        let currentIndex = (startIndex + i) % game.playerCount;
-        let currentPlayer = game.players[currentIndex];
-
-        if (currentPlayer.inHand && currentPlayer.chips > 0) {
-            return currentIndex;
-        }
-    }
-
-    return -1;
-}
-
-function advanceGameStage(game) {
-    game.players.forEach(player => {
-        player.bet = 0;
-        player.hasActed = !player.inHand;
-    });
-    game.highestBet = 0;
-    game.bettingStarted = false;
-    game.minRaiseAmount = game.initialBigBlind;
-
-    switch (game.gameStage) {
-        case GAME_STAGES.PRE_FLOP:
-            dealCommunityCards(game, 3);
-            game.gameStage = GAME_STAGES.FLOP;
-            break;
-        case GAME_STAGES.FLOP:
-            dealCommunityCards(game, 1);
-            game.gameStage = GAME_STAGES.TURN;
-            break;
-        case GAME_STAGES.TURN:
-            dealCommunityCards(game, 1);
-            game.gameStage = GAME_STAGES.RIVER;
-            break;
-        case GAME_STAGES.RIVER:
-            determineWinner(game); // Ensure this function saves the updated game state
-            return; // Exit after determining the winner, since it should handle saving and broadcasting the game state
-    }
-
-    if (game.gameStage !== GAME_STAGES.GAME_OVER) {
-        game.currentTurn = findFirstActivePlayer(game);
-    }
-}
-
-function allPlayersHaveActed(game) {
-    const isInitialRound = game.gameStage === GAME_STAGES.PRE_FLOP;
-
-    const activePlayers = game.players.filter(player => player.inHand);
-
-    // Check if all active players have matched the highest bet or are all-in
-    const allMatchedOrAllIn = activePlayers.every(player =>
-        player.chips === 0 ||
-        player.bet === game.highestBet
-    );
-
-    if (isInitialRound) {
-        const bigBlindIndex = (game.smallBlindIndex + 1) % game.players.length;
-        const bigBlindPlayer = game.players[bigBlindIndex];
-
-        // Check if the big blind has had the opportunity to act
-        const bigBlindHadOpportunityToAct = bigBlindPlayer && (bigBlindPlayer.hasActed || bigBlindPlayer.bet !== game.highestBet);
-
-        // For the initial round, all players must have matched the highest bet or be all-in,
-        // and the big blind must have had the opportunity to act
-        return allMatchedOrAllIn && bigBlindHadOpportunityToAct;
-    } else {
-        // For subsequent rounds, check if betting has started
-        if (game.bettingStarted) {
-            // If betting has started, all players must have matched the highest bet or be all-in
-            return allMatchedOrAllIn;
-        } else {
-            // If betting hasn't started, check if all players have acted (checked or folded)
-            return game.players.every(player =>
-                !player.inHand ||
-                player.hasActed
-            );
-        }
-    }
-}
 
 exports.handler = async (event) => {
     const { gameId, playerId } = JSON.parse(event.body);
@@ -276,14 +34,14 @@ exports.handler = async (event) => {
         const playersInHand = game.players.filter(player => player.inHand);
         if (playersInHand.length === 1) {
             game.gameStage = 'gameOver';
-            distributePotsForFolding(game); // Ensure this function saves the updated game state
+            Helper.distributePotsForFolding(game); // Ensure this function saves the updated game state
         } else {
-            const allInConditionMet = checkAllInCondition(game);
+            const allInConditionMet = Helper.checkAllInCondition(game);
             if (!allInConditionMet) {
-                if (allPlayersHaveActed(game)) {
-                    advanceGameStage(game); // Ensure this function saves the updated game state
+                if (Helper.allPlayersHaveActed(game)) {
+                    Helper.advanceGameStage(game); // Ensure this function saves the updated game state
                 } else {
-                    advanceTurn(game); // Ensure this function saves the updated game state
+                    Helper.advanceTurn(game); // Ensure this function saves the updated game state
                 }
             }
         }
@@ -302,7 +60,7 @@ exports.handler = async (event) => {
         return { statusCode: 500, body: JSON.stringify({ message: error.message }) };
     }
 };
-//
+
 async function getGameState(gameId) {
     const params = {
         TableName: gameTableName,
@@ -316,7 +74,18 @@ async function saveGameState(gameId, game) {
     const params = {
         TableName: gameTableName,
         Key: { gameId },
-        UpdateExpression: "SET players = :p, gameOverTimeStamp = :gOTS, bettingStarted = :bS, minRaiseAmount = :mRA, deck = :deck, pot = :pot, gameStage = :gs, currentTurn = :ct, communityCards = :cc, highestBet = :hb, gameInProgress = :gip, netWinners = :nw",
+        UpdateExpression: `SET players = :p, 
+            gameOverTimeStamp = :gOTS, 
+            bettingStarted = :bS, 
+            minRaiseAmount = :mRA, 
+            deck = :deck, 
+            pot = :pot, 
+            gameStage = :gs, 
+            currentTurn = :ct, 
+            communityCards = :cc, 
+            highestBet = :hb, 
+            gameInProgress = :gip, 
+            netWinners = :nw`,
         ExpressionAttributeValues: {
             ":p": game.players,
             ":gOTS": game.gameOverTimeStamp,
